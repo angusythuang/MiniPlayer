ef2669e1b4872d5b69335934f46a0edbd8332a5e
angusythuang <angusythuang@gmail.com>
2025-08-18 17:52:52 +0800

    新增右鍵的 Context menu，目前只有一個 Delete，且未與系統API連接

---
+++ MiniPlayer/FileBrowser/ContextMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace MiniPlayer
+{
+    public partial class MainWindow
+    {
+        // ListViewItem_MouseRightButtonDown 右鍵點擊刪除
+        private void MenuItem_Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (lvFileList.SelectedItem == null)
+            {
+                MessageBox.Show($"{DebugInfo.Current()} 請先選擇要刪除的項目。", "錯誤", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // 直接拿 lvFileList 的選中項目
+            FileSystemItem f = (FileSystemItem)lvFileList.SelectedItem;
+
+            DebugInfo.PrintDebugMsg($"刪除：{f.FullPath}");
+        }
+    }
+}
