97b6f891adb59b4713d6c7a8db8cb1fdd5864269
angusythuang <angusythuang@gmail.com>
2025-08-18 18:00:38 +0800

    DebugInfo獨立出來，並且在 DEBUG 才會印訊息。

---
+++ MiniPlayer/DebugInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MiniPlayer
+{
+    public static class DebugInfo
+    {
+        public static string Current(
+            [CallerFilePath] string file = "",
+            [CallerLineNumber] int line = 0,
+            [CallerMemberName] string func = "")
+        {
+            return $"[{Path.GetFileName(file)}:{line} - {func}]";
+        }
+
+        public static void PrintDebugMsg(string message)
+        {
+#if DEBUG
+            // 使用 Debug.WriteLine 來輸出訊息
+            System.Diagnostics.Debug.WriteLine($"{message}");
+#endif
+        }
+    }
+}
