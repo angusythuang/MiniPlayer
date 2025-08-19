cea2b6a724c2f45ccded74e116b7eac2aa3d3b85
angusythuang <angusythuang@gmail.com>
2025-08-19 16:05:12 +0800

    定義 Context Menu 有 剪下 複製 貼上 三個行為

--- MiniPlayer/FileBrowser/ContextMenu.cs
+++ MiniPlayer/FileBrowser/ContextMenu.cs
@@ -21,17 +21,26 @@ namespace MiniPlayer
 
         private void MenuItem_Paste_Click(object sender, RoutedEventArgs e)
         {
-            if (lvFileList.SelectedItem == null)
-            {
-                MessageBox.Show($"{DebugInfo.Current()} 請先選擇要刪除的項目。", "錯誤", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
-
             // 直接拿 lvFileList 的選中項目
             FileSystemItem f = (FileSystemItem)lvFileList.SelectedItem;
 
             DebugInfo.PrintDebugMsg($"貼上：{f.FullPath}");
         }
-        
+
+        private void MenuItem_Cut_Click(object sender, RoutedEventArgs e)
+        {
+            // 直接拿 lvFileList 的選中項目
+            FileSystemItem f = (FileSystemItem)lvFileList.SelectedItem;
+
+            DebugInfo.PrintDebugMsg($"剪下：{f.FullPath}");
+        }
+
+        private void MenuItem_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            // 直接拿 lvFileList 的選中項目
+            FileSystemItem f = (FileSystemItem)lvFileList.SelectedItem;
+
+            DebugInfo.PrintDebugMsg($"複製：{f.FullPath}");
+        }   
     }
 }
