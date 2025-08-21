fb69ba05f337c68fcf812d60b6e03eb4a8c5b758
angusythuang <angusythuang@gmail.com>
2025-08-21 14:15:47 +0800

    增加註解

--- MiniPlayer/MainWindow.xaml.cs
+++ MiniPlayer/MainWindow.xaml.cs
@@ -8,7 +8,6 @@ using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-//using System.Windows.Forms;
 using System.Windows.Media;
 using System.Windows.Threading;
 using SWF = System.Windows.Forms;
@@ -20,7 +19,7 @@ namespace MiniPlayer
     /// </summary>
     public partial class MainWindow : Window
     {
-        // 當前目錄
+        // 當前目錄，設定為屬性，讓 XAML 可以綁定
         public CurrentDir CurrentDir { get; set; }= new CurrentDir();        
 
         // 用於監聽磁碟機變更的物件
